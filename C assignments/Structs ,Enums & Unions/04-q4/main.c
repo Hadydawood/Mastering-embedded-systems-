f90d758861ccf84ccc0fa35494e0921cd93cd422
Hadydawood <hady84852002@gmail.com>
2025-04-10 16:07:21 +0200

    ====

--- C assignments/Structs ,Enums & Unions/04-q4/main.c
+++ C assignments/Structs ,Enums & Unions/04-q4/main.c
@@ -1,34 +1,47 @@
 #include <stdio.h>
-struct students {
-    char name[50];
-    int roll_number;
-    int marks;
+// struct students {
+//     char name[50];
+//     int roll_number;
+//     int marks;
+// };
+// struct students read_data(int x) {
+//     struct students a;
+//     printf("Enter information for roll number %d:\n", x + 1);
+//     printf("Name: ");
+//     scanf("%s", a.name);
+//     printf("Marks: ");
+//     scanf("%d", &a.marks);
+//     a.roll_number = x + 1;
+//     return a;
+// }
+// void display(int x, struct students a) {
+//     printf("Information for roll number %d:\n", x + 1);
+//     printf("Name: %s\n", a.name);
+//     printf("Marks: %d\n", a.marks);
+// }
+// int main(void) {
+//          int *intPtr;
+//         char *charPtr;
+//         double *doublePtr;
+//         void *voidPtr;
+//         void (*funcPtr)(); // Function pointer
+//
+//         printf("Size of int pointer: %zu bytes\n", sizeof(intPtr));
+//         printf("Size of char pointer: %zu bytes\n", sizeof(charPtr));
+//         printf("Size of double pointer: %zu bytes\n", sizeof(doublePtr));
+//         printf("Size of void pointer: %zu bytes\n", sizeof(voidPtr));
+//         printf("Size of function pointer: %zu bytes\n", sizeof(funcPtr));
+//
+//         return 0;
+// }
+typedef unsigned int Uint32_t;
+struct Sperson {
+    Uint32_t age ;
 };
-struct students read_data(int x) {
-    struct students a;
-    printf("Enter information for roll number %d:\n", x + 1);
-    printf("Name: ");
-    scanf("%s", a.name);
-    printf("Marks: ");
-    scanf("%d", &a.marks);
-    a.roll_number = x + 1;
-    return a;
-}
-void display(int x, struct students a) {
-    printf("Information for roll number %d:\n", x + 1);
-    printf("Name: %s\n", a.name);
-    printf("Marks: %d\n", a.marks);
-}
-int main(void) {
-    struct students b[10];
-    int students_number = 10, i;
-    printf("Enter the details of the students:\n");
-    for (i = 0; i < students_number; i++) {
-        b[i] = read_data(i);
-    }
-    printf("\nDisplaying the information of students:\n");
-    for (i = 0; i < students_number; i++) {
-        display(i, b[i]);
-    }
+typedef struct Sperson Sperson_t;
+int main() {
+    Uint32_t x ;
+    Sperson_t y ;
+
     return 0;
 }
--- C assignments/Structs ,Enums & Unions/05-q5/main.c
+++ C assignments/Structs ,Enums & Unions/05-q5/main.c
@@ -1,42 +1,13 @@
-#include <stdbool.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-
-char* decimal_to_binary(int x) {
-	static char binary[100];
-	int length = 0;
-	if (x == 0) {
-		binary[length++] = '0';
-	} else {
-		while (x > 0) {
-			if (x % 2 == 0)
-				binary[length++] = '0';
-			else
-				binary[length++] = '1';
-			x /= 2;
-		}
-	}
-	return binary;
-}
-int main(){
-	int decimal ;
-	printf("Enter the decimal number ");
-	fflush(stdin);fflush(stdout);
-	scanf("%d",&decimal);
-
-	int ones_count = 0 ;
-	if (decimal < 0 ){
-		printf("please enter a non-negative number \n");
-	}else {
-		char*binary = decimal_to_binary(decimal);
-		for (int i =0 ; binary[i]!='\0';i++){
-			if (binary[i]=='1'){
-				ones_count+=1;
-			}
-		}
-
-	}
-	printf("the numbers of 1s in the binary representation of %d is %d",decimal , ones_count);
-	return 0 ;
+#include<stdio.h>
+#define PI 3.1415
+#define area(r) (PI*(r)*(r))
+struct emp {
+	int age ;
+	char name[50];
+	float bs ;
+};
+int main()
+{
+	struct emp s ;
+	strcpy(s.name,"hady");
 }
